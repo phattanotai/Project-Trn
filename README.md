c4f44b509af09f69678e705b442f87282422f640
phattanothai <zeref38@gmail,com>
2022-08-08 21:37:03 +0700

    update deposit withdraw

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # trn
 
+docker-compose build
+
+docker-compose up -d
+
+
+username staff@gmail.com
+password 1234
--- www/config/web-constant.js
+++ www/config/web-constant.js
@@ -1,6 +1,15 @@
+// const API = {
+//    auth: `http://localhost:30000`,
+//    staff: `http://localhost:31000`,
+//    member: `http://localhost:32000`,
+//    masterData: `http://localhost:35000`
+// };
+
 const API = {
-   auth: `http://localhost:30000`,
-   staff: `http://localhost:31000`,
-   member: `http://localhost:32000`,
-   masterData: `http://localhost:35000`
+  auth: `http://localhost/api/auth`,
+  staff: `http://localhost/api/staff`,
+  member: `http://localhost/api/member`,
+  masterData: `http://localhost/api/masterData`,
+  withdraw: "http://localhost/api/withdraw",
+  deposit: "http://localhost/api/deposit",
 };
